5926194310e3e0457bf87be7a2447183d88e7316
Wesley T. Benette <weluonf@gmail.com>
2024-06-25 22:57:45 -0300

    ✨ feat: Creates and applies FooterComponent

--- src/app/views/pages/home-page/home-page.component.ts
+++ src/app/views/pages/home-page/home-page.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../../components/footer/footer.component';
 
-@Component({
-  selector: 'app-home-page',
-  standalone: true,
-  imports: [],
-  templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.scss'
+@Component
+({
+	selector: 'app-home-page',
+	standalone: true,
+	imports: [FooterComponent],
+	templateUrl: './home-page.component.html',
+	styleUrl: './home-page.component.scss'
 })
-export class HomePageComponent {
+export class HomePageComponent
+{
 
 }
