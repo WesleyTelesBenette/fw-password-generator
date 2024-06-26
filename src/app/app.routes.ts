39d0ba1599ba8a8d5dcf03639750be3a7758fcde
Wesley T. Benette <weluonf@gmail.com>
2024-06-25 22:43:32 -0300

    ✨ feat: Creates HomePageComponent and Layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './views/pages/home-page/home-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes =
+[
+	{ path: '', component: HomePageComponent}
+];
+
+
+@NgModule
+({
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
+})
+export class AppRoutingModule {}
