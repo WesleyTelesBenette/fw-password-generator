0896ab40575870fe08d41298faae427784616ad4
Wesley T. Benette <weluonf@gmail.com>
2024-07-09 23:02:59 -0300

    ✨ feat: Adds AlertComponent

---
+++ src/app/views/components/alert/alert.component.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-alert',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './alert.component.html',
+  styleUrl: './alert.component.scss'
+})
+export class AlertComponent
+{
+	@Input() content: string = '';
+	@Input() onAlert: boolean | any;
+}
--- src/app/views/components/result/result.component.ts
+++ src/app/views/components/result/result.component.ts
@@ -1,10 +1,12 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { AlertComponent } from '../alert/alert.component';
+import { timeout } from 'rxjs';
 
 @Component
 ({
 	selector: 'app-result',
 	standalone: true,
-	imports: [],
+	imports: [AlertComponent],
 	templateUrl: './result.component.html',
 	styleUrl: './result.component.scss'
 })
@@ -12,14 +14,20 @@ export class ResultComponent
 {
 	@Output() action: EventEmitter<any> = new EventEmitter<any>();
 	@Input() password: string = '';
+	public activeAlert: boolean | any;
 
 	public callAction()
 	{
 		this.action.emit();
 	}
 
-	public copyPassword()
+	public async copyPassword()
 	{
 		navigator['clipboard'].writeText(this.password).then().catch(() => console.error('Erro ao copiar senha...'));
+		this.activeAlert = true;
+		console.log('Alert');
+		await new Promise(r => setTimeout(r, 5000));
+		console.log('Close Alert');
+		this.activeAlert = false;
 	}
 }
